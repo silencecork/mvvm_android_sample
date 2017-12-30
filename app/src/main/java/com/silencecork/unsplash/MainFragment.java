613c2a556e427e98ad1858748662ed7b2a68cdb5
justin <silencecork@gmail.com>
2017-12-31 01:07:23 +0800

    add comment and update variable

--- app/src/main/java/com/silencecork/unsplash/MainFragment.java
+++ app/src/main/java/com/silencecork/unsplash/MainFragment.java
@@ -24,10 +24,112 @@ import java.util.List;
 
 public class MainFragment extends Fragment {
 
-    FragmentMainBinding mBinding;
-    MainViewModel mainViewModel;
-    FeatureListAdapter mFeatureListAdapter;
-    int page = 1;
+    private FragmentMainBinding mBinding;
+    private MainViewModel mMainViewModel;
+    private FeatureListAdapter mFeatureListAdapter;
+    private int mCurrentPage = 1;
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        // Create ViewModel use ViewModelProviders
+        mMainViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
+
+        // isLoading defined in xml
+        mBinding.setIsLoading(true);
+
+        // get LiveData from ViewModel and register the observer
+        mMainViewModel.getCurrentFeaturedList().observe(this, new Observer<List<Collection>>() {
+            @Override
+            public void onChanged(@Nullable List<Collection> collections) {
+                // when change completed, stop loading
+                mBinding.setIsLoading(false);
+
+                // set data to adapter
+                mFeatureListAdapter.setData(collections);
+            }
+        });
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // binding class name will be the layout file name. e.g. fragment_main -> FragmentMainBinding
+        mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_main, container, false);
+
+        // get binding root view
+        return mBinding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mFeatureListAdapter = new FeatureListAdapter();
+
+        // add scroller for pagination
+        mBinding.list.addOnScrollListener(mOnScrollListener);
+
+        // set adapter
+        mBinding.list.setAdapter(mFeatureListAdapter);
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // call api load data
+        loadFeatureList(mCurrentPage);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+
+        // remove scroller
+        if (mBinding != null) {
+            mBinding.list.removeOnScrollListener(mOnScrollListener);
+        }
+
+        // remove observer
+        if (mMainViewModel != null) {
+            mMainViewModel.getCurrentFeaturedList().removeObservers(this);
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+
+
+    private void loadFeatureList(int page) {
+        Log.i("Main", "load mCurrentPage " + page);
+        // start show loading indicator
+        mBinding.setIsLoading(true);
+
+        // call api with page parameters
+        mMainViewModel.getObservableFeaturedList(page);
+    }
+
     private RecyclerView.OnScrollListener mOnScrollListener = new RecyclerView.OnScrollListener() {
         @Override
         public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
@@ -45,87 +147,9 @@ public class MainFragment extends Fragment {
             if (!mBinding.getIsLoading()) {
                 if ((visibleItemCount + firstVisibleItemPosition) >= totalItemCount
                         && firstVisibleItemPosition >= 0) {
-                    loadFeatureList(++page);
+                    loadFeatureList(++mCurrentPage);
                 }
             }
         }
     };
-
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-
-        mainViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
-        mBinding.setIsLoading(true);
-        mBinding.executePendingBindings();
-        mainViewModel.getCurrentFeaturedList().observe(this, new Observer<List<Collection>>() {
-            @Override
-            public void onChanged(@Nullable List<Collection> collections) {
-                mBinding.setIsLoading(false);
-                if (mFeatureListAdapter == null) {
-                    mFeatureListAdapter = new FeatureListAdapter();
-                    mBinding.list.setAdapter(mFeatureListAdapter);
-                }
-                Log.i("Main", "data size " + collections.size());
-                mFeatureListAdapter.setData(collections);
-                mFeatureListAdapter.notifyDataSetChanged();
-//                for (Collection collection : collections) {
-//                    Log.i("LiveData", collection.toString());
-//                }
-            }
-        });
-    }
-
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_main, container, false);
-        return mBinding.getRoot();
-    }
-
-    @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        mBinding.list.addOnScrollListener(mOnScrollListener);
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-
-        loadFeatureList(page);
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-    }
-
-    private void loadFeatureList(int page) {
-        Log.i("Main", "load page " + page);
-        mBinding.setIsLoading(true);
-        mBinding.executePendingBindings();
-        mainViewModel.getObservableFeaturedList(page);
-    }
-
 }
